42758650f92cdab85d5f6a5db215ffc1747924b0
toponeprofile <toponeprofile@gmail.com>
2021-09-01 23:20:19 +0300

    Update

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set user  
+git config --local credential.helper ""
+git config --local user.name "toponeprofile"
+git config --local user.email "toponeprofile@gmail.com"
+
+# Commit and push
+git checkout main
+git add .
+git commit -m "Update"
+git push --set-upstream origin main
